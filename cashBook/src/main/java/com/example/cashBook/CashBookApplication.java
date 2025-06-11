dbb3487cd1462b3d1d0187f5b7ab691180f521d1
ELISHA MLYASENDE <elisha.mlyasend@imarishamaisha.co.tz>
2025-06-11 16:52:53 +0300

    just i have added ureka configuration in auth, cashbook, officeflow service

--- cashBook/src/main/java/com/example/cashBook/CashBookApplication.java
+++ cashBook/src/main/java/com/example/cashBook/CashBookApplication.java
@@ -2,8 +2,10 @@ package com.example.cashBook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CashBookApplication {
 
 	public static void main(String[] args) {
