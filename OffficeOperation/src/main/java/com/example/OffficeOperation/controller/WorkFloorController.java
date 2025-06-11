a303c2de599515ed6ae0dadba3970b47acacb588
ELISHA MLYASENDE <elisha.mlyasend@imarishamaisha.co.tz>
2025-06-11 13:55:25 +0300

    I completed the workflow Controller for Client

--- OffficeOperation/src/main/java/com/example/OffficeOperation/controller/WorkFloorController.java
+++ OffficeOperation/src/main/java/com/example/OffficeOperation/controller/WorkFloorController.java
@@ -1,10 +1,37 @@
 package com.example.OffficeOperation.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.OffficeOperation.model.workFloor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.example.OffficeOperation.service.workFloorService;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/Client/api")
 public class WorkFloorController {
-
+  private final workFloorService workFloorService;
+  public WorkFloorController(workFloorService workFloorService){
+    this.workFloorService=workFloorService;
+  }
+  @PostMapping("/add")
+  public ResponseEntity<?>addClient(@RequestBody workFloor workFloor){
+    return workFloorService.addWorkFlow(workFloor);
+  }
+  @PutMapping("/edit")
+  public  ResponseEntity<?> editClient(@RequestParam Long id, @RequestBody workFloor workFloor){
+    return workFloorService.editWorkFlow(workFloor,id);
+  }
+  @DeleteMapping("/delete/{id}")
+  public ResponseEntity<?> deleteClient(@PathVariable("id")Long id, @RequestBody workFloor workFloor){
+    return workFloorService.deleteWorkFlow(workFloor,id);
+  }
+  @GetMapping("/allClient")
+  public List<workFloor> readAllClient(){
+    return workFloorService.getAllClientWithworks();
+  }
+  @GetMapping("/allClient/{id}")
+  public ResponseEntity<?> getClientById(@PathVariable("id") Long id){
+    return workFloorService.getClientById(id);
+  }
 }
