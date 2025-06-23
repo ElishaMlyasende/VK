018e523d29778a70ff520ff52754b69c1a99e390
ELISHA MLYASENDE <elisha.mlyasend@imarishamaisha.co.tz>
2025-06-23 15:21:13 +0300

    done with cashbook in controller

--- cashBook/src/main/java/com/example/cashBook/controller/mobilePettyCashController.java
+++ cashBook/src/main/java/com/example/cashBook/controller/mobilePettyCashController.java
@@ -1,7 +1,38 @@
 package com.example.cashBook.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.cashBook.implement.mobileTransaction;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.example.cashBook.service.mobilePettyCashBook;
+import com.example.cashBook.model.mobilePettyCash;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("cashbook/mobile")
 public class mobilePettyCashController {
+    private final mobilePettyCashBook mobilePettyCashBook;
+    public mobilePettyCashController(mobilePettyCashBook mobilePettyCashBook){
+        this.mobilePettyCashBook=mobilePettyCashBook;
+    }
+    @PostMapping("/add")
+    public ResponseEntity<?> postAllMobileTranasaction(mobilePettyCash saved){
+        return  mobilePettyCashBook.addMobilePettyCash(saved);
+    }
+    @PutMapping("/edit")
+    public ResponseEntity<?>editMobilePettyCash(@RequestParam Long id,mobilePettyCash updated){
+        return mobilePettyCashBook.updateMobilePettyCash(updated, id);
+    }
+    @DeleteMapping("/delete/{id}")
+    public  ResponseEntity<?> deleteMobilePettyCash(@PathVariable("id")Long id){
+        return mobilePettyCashBook.deleteMobilePettyCash(id);
+    }
+    @GetMapping("/all")
+    public List<mobilePettyCash>getAllMobilePettyCash(){
+        return mobilePettyCashBook.gettAllTransaction();
+    }
+    @GetMapping("/{id}")
+    public  ResponseEntity<?> getMobileTransactionById(@PathVariable("id")Long id){
+        return mobilePettyCashBook.getTrasactionById(id);
+    }
 }
--- cashBook/src/main/java/com/example/cashBook/controller/pettyCashController.java
+++ cashBook/src/main/java/com/example/cashBook/controller/pettyCashController.java
@@ -1,7 +1,38 @@
 package com.example.cashBook.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.example.cashBook.service.pettyCashService;
+import com.example.cashBook.model.pettyCash;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/cashbook")
 public class pettyCashController {
+    private final pettyCashService pettyCashService;
+    public pettyCashController(pettyCashService pettyCashService){
+        this.pettyCashService=pettyCashService;
+    }
+    @PostMapping("/add")
+    public ResponseEntity<?>addPettyCash(@RequestBody pettyCash savedPettyCash){
+        return pettyCashService.savePettyCash(savedPettyCash);
+    }
+    @GetMapping("/all")
+    List<pettyCash>listAllpettyCash(){
+        return pettyCashService.getAllPettyCash();
+    }
+    @PutMapping("/edit")
+    public  ResponseEntity<?> updatePettyCash(@RequestParam Long id, @RequestBody pettyCash updated){
+        return pettyCashService.updatePettyCash(updated, id);
+    }
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> deletePettyCash(@RequestParam Long id){
+        return  pettyCashService.deletePettyCash(id);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getPettyCashById(@PathVariable("id") Long id){
+        return pettyCashService.getPettyCashById(id);
+    }
+
 }
